3335f736a00b3ead722267a06d3c1fe2ab348763
Molly Seeley <mseeley89@gmail.com>
2020-04-07 09:34:40 -0400

    notes js + style animation

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
 const items = document.querySelectorAll('#timeline li');
 
+// checks if the element is in the viewport
+const isInViewport = el => {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// runs through each item of the DOM and assigns annimation class "show" if item is in the viewport
+const run = () => items.forEach(item => {
+  if (isInViewport(item)) item.classList.add('show');
+});
+
 /*
+conditions underwhich run will be triggered
+  on page load, page resize, and page scroll
+*/
+
+window.addEventListener('load', run);
+window.addEventListener('resize', run);
+window.addEventListener('scroll', run)
+
+
+/*
+NOTES
 Element.getBoundingClientRect()
   returns the size of an element and its position relative to the viewport
 
@@ -21,29 +48,3 @@ document.documentElement
     clientWidth
       Returns a Number representing the inner width of the element.
 */
-
-// checks if the element is in the viewport
-const isInViewport = el => {
-  const rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <=
-    (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
-
-// runs through each item of the DOM and assigns annimation class "show" if item is in the viewport
-const run = () => items.forEach(item => {
-  if (isInViewport(item)) item.classList.add('show');
-});
-
-/*
-conditions underwhich run will be triggered
-  on page load, page resize, and page scroll
-*/
-
-window.addEventListener('load', run);
-window.addEventListener('resize', run);
-window.addEventListener('scroll', run)
